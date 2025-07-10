fe6be38527d2209543f95af0a30153ae5af3e735
Jeffry <jeffry1408.job@gmail.com>
2025-07-10 10:47:59 +0700

    change into jsx

---
+++ combined-specific/awwwards-winning-website-react-tailwind-gsap/src/constants/index.jsx
@@ -0,0 +1,58 @@
+export const navItems = [
+  { name: "Nexus", target: "nexus" },
+  { name: "Vault", target: "vault" },
+  { name: "Prologue", target: "prologue" },
+  { name: "About", target: "about" },
+  { name: "Contact", target: "contact" },
+];
+
+export const navCollapseDuration = 125;
+
+export const audioButtonLines = [1, 2, 3, 4];
+
+export const featuresData = [
+  {
+    id: 0,
+    src: "videos/feature-1.mp4",
+    title: (
+      <>
+        radi<b>a</b>nt
+      </>
+    ),
+    description:
+      "A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure.",
+  },
+  {
+    id: 1,
+    src: "videos/feature-2.mp4",
+    title: (
+      <>
+        zig<b>m</b>a
+      </>
+    ),
+    description:
+      "An anime and gaming-inspired NFT collection—the IP primed for expansion.",
+  },
+  {
+    id: 2,
+    src: "videos/feature-3.mp4",
+    title: (
+      <>
+        n<b>e</b>xus
+      </>
+    ),
+    description:
+      "A gamified social hub, adding a new dimension of play to social interaction for Web3 communities.",
+  },
+  {
+    id: 3,
+    src: "videos/feature-4.mp4",
+    title: (
+      <>
+        az<b>u</b>l
+      </>
+    ),
+    description:
+      "A cross-world AI Agent—elevating your gameplay to be more fun and productive.",
+  },
+];
