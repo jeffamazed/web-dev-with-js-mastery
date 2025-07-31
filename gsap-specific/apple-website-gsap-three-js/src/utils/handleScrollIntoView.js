1d2821aa119dd85a4d13cb2682c4b6a49fb72523
jeffamazed <jeffry1408.job@gmail.com>
2025-07-31 21:40:14 +0700

    feat(handleScrollIntoView.js): add prefer reduced motion logic for util func

--- gsap-specific/apple-website-gsap-three-js/src/utils/handleScrollIntoView.js
+++ gsap-specific/apple-website-gsap-three-js/src/utils/handleScrollIntoView.js
@@ -2,11 +2,20 @@ const handleScrollIntoView = (e, navRef, sectionRef) => {
   const section = sectionRef.current;
   const nav = navRef.current;
   if (!section || !nav) return;
-  e.preventDefault();
-  const navHeight = nav.getBoundingClientRect().height;
-  const sectionTop = section.getBoundingClientRect().top + scrollY;
 
-  window.scrollTo({ top: sectionTop - navHeight, behavior: "smooth" });
+  e.preventDefault();
+
+  const navHeight = nav.getBoundingClientRect().height;
+  const sectionTop = section.getBoundingClientRect().top + window.scrollY;
+
+  const prefersReducedMotion = window.matchMedia(
+    "(prefers-reduced-motion: reduce)",
+  ).matches;
+
+  window.scrollTo({
+    top: sectionTop - navHeight,
+    behavior: prefersReducedMotion ? "auto" : "smooth",
+  });
 };
 
 export default handleScrollIntoView;
