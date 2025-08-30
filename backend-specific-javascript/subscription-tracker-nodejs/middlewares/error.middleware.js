03eaa33b79944edab5b279986675a1db8e06f317
jeffamazed <jeffry1408.job@gmail.com>
2025-08-30 09:36:46 +0700

    feat: add global error handler

---
+++ backend-specific-javascript/subscription-tracker-nodejs/middlewares/error.middleware.js
@@ -0,0 +1,48 @@
+import CustomAPIError from "../classes/CustomAPIError.js";
+
+const errorMiddleware = (err, req, res, next) => {
+  // validation error
+  if (err.name === "ValidationError") {
+    const error = Object.values(err.errors).map((e) => ({
+      field: e.path,
+      message: e.message,
+    }));
+    console.error(error);
+    return res.status(400).json({ success: false, error });
+  }
+
+  // cast error
+  if (err.name === "CastError") {
+    const error = `${err.value} is not a valid ${err.kind} for field ${err.path}.`;
+
+    console.error(error);
+
+    return res.status(400).json({ success: false, error });
+  }
+
+  // duplicate keys error
+  if (err.code === 11000) {
+    const fields = Object.keys(err.keyPattern);
+
+    const error = fields.map((f) => ({
+      field: f,
+      message: `Duplicate value for ${f}: ${err.keyValue[f]}.`,
+    }));
+
+    return res.status(400).json({ success: false, error });
+  }
+
+  // custom api error
+  if (err instanceof CustomAPIError) {
+    console.error(err.logMessage);
+    return res
+      .status(err.statusCode)
+      .json({ success: false, error: err.message });
+  }
+
+  // fallback error
+  console.error(err);
+  res.status(500).json({ success: false, error: "Internal server error." });
+};
+
+export default errorMiddleware;
