4624d70106fab5b0d6efef6cf4fe8e37a4442ac9
jeffamazed <jeffry1408.job@gmail.com>
2025-08-30 08:15:24 +0700

    feat: implement mongodb models
     - feat: implement User model
     - feat: implement Subscription model

---
+++ backend-specific-javascript/subscription-tracker-nodejs/models/user.model.js
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required."],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "User email is required."],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
+        "Please fill a valid email address.",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "User password is required."],
+      minLength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", UserSchema);
+export default User;
