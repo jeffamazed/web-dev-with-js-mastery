040a1179e2c7da017b66dcb7bbefcb3e4843f282
jeffamazed <jeffry1408.job@gmail.com>
2025-08-31 10:40:47 +0700

    fix: use more appropriate message for unauthorized

--- backend-specific-javascript/subscription-tracker-nodejs/middlewares/auth.middleware.js
+++ backend-specific-javascript/subscription-tracker-nodejs/middlewares/auth.middleware.js
@@ -27,9 +27,9 @@ const authorize = async (req, res, next) => {
   const user = await User.findById(decoded.userId);
   if (!user) {
     throw new CustomAPIError(
-      "Unauthorized.",
+      "Unauthorized. User does not exist.",
       401,
-      `User with userId ${decoded.userId} is not found.`
+      `User with userId ${decoded.userId} tries to access, but does not exist.`
     );
   }
 
